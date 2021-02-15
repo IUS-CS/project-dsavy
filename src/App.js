027135aad31a0b516b484aa8ed279fbec12b1f6e
Justin Hurst <hurstjustin502@gmail.com>
2021-02-15 15:49:22 -0500

    added react routing and nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About"
 
 export default function App() {
     return (
-      <div>
-        <h1>Welcome to DSAVY!</h1>
-      </div>
+      <Router>
+        <Navigation/>
+        <Route path='/' component={Home} exact/>
+        <Route path='/about' component={About} />
+      </Router>
     );
   }
