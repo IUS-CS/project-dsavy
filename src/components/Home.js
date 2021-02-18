3f7eef58dfcb1ae2b5c682ef0d1fb34f8d70bfae
zkhussain <kzhzaid02@gmail.com>
2021-02-18 14:35:35 -0500

    add card components on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import ClickableImage from './Home-image';
+import Card from './Card.js'
 
 function Home() {
   return (
-      <Container style={{paddingTop:20}}>
-        <Row style={{padding:10}}>
+      <Container style={{paddingTop:20}} className="container-fluid d-flex justify-content-center">
+        <Row>
+            <Col>
+                <Card name="Linked Lists" image="../images/LinkedList.png" page='/linkedlist'
+                    description="A linear data structure in which a node has a value and a pointer to next node"/>
+            </Col>
+            <Col>
+                <Card name="Stacks" image="../images/LinkedList.png" />
+            </Col>
+            <Col>
+                <Card name="Queues" image="../images/LinkedList.png" />
+            </Col>
+        </Row>
+
+        {/* <Row style={{padding:10}}>
           <Col><ClickableImage name='Linked List' image='../images/LinkedList.png' page='/linkedlist'/></Col>
           <Col><ClickableImage name='Stack' image='../images/LinkedList.png' page='/about'/></Col>
           <Col><ClickableImage name='Data Structure' image='../images/LinkedList.png' page='/about'/></Col>
@@ -14,7 +28,7 @@ function Home() {
         <Col><ClickableImage name='Linked List' image='../images/LinkedList.png' page='/about'/></Col>
           <Col><ClickableImage name='Linked List' image='../images/LinkedList.png' page='/about'/></Col>
           <Col><ClickableImage name='Linked List' image='../images/LinkedList.png' page='/about'/></Col>
-        </Row>
+        </Row> */}
       </Container>
   );
 }
