a84f7770c0bda8c815adb0f86a3f604377c85339
Jason Henry <jasdhenr@iu.edu>
2021-03-02 17:34:10 -0500

    added queue dequeue and enqueue

---
+++ src/components/Enqueue.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import { Button} from 'react-bootstrap';
+
+const Enqueue = ({enqueue}) => {
+  const [entry, setEntry] = useState('')
+  
+  const onSubmit = (e) => {
+      e.preventDefault()
+      enqueue({entry})
+      setEntry('')
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Button type="submit" className="my-1">Enqueue</Button>
+      <input className='enqueue' type='number' value={entry} onChange={(e) => setEntry(e.target.value)} />
+    </form>
+  )
+}
+
+export default Enqueue
