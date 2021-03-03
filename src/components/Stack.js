462f83d9680f274025b3a70655b66a49b69adfe9
zkhussain <kzhzaid02@gmail.com>
2021-03-02 23:00:15 -0500

    add Stack component

---
+++ src/components/Stack.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import { useSpring, animated } from 'react-spring';
+import StackForm from './Stack-Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Stack = () => {
+    return (
+        <animated.div>
+            <StackForm />
+
+        </animated.div>
+    );
+}
+
+export default Stack
