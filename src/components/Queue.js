3c7efc3567e195beca20190e36f7e7bdf41ef65c
Jason Henry <jasdhenr@iu.edu>
2021-03-16 17:11:25 -0400

    formatting

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -22,10 +22,10 @@ const Queue = () => {
   }
   
   const transition = useTransition(queue, entry => entry.key, {
-    initial: { opacity: 0, display: 'inline'},
-    from: { opacity: 0, display: 'inline' },
-    enter: { opacity: 1, transition: 'opacity .5s'},
-    leave: { opacity: 0, transition: 'opacity .5s' }, 
+    initial: {opacity: 0, display: 'inline'},
+    from: {opacity: 0, display: 'inline' },
+    enter: {opacity: 1, transition: 'opacity .5s'},
+    leave: {opacity: 0, transition: 'opacity .5s' }, 
     
   })
 
