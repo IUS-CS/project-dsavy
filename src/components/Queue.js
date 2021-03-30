26aac6bdfc2f97b16dd6ad5fe4365fa0d63fe11a
Jason Henry <jasdhenr@iu.edu>
2021-03-29 21:47:11 -0400

    added style to queue and error checks

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,15 +1,40 @@
 import React, {useState} from 'react'
 import {animated, useTransition} from 'react-spring'
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, ListGroup} from 'react-bootstrap';
 import Enqueue from './Forms/Enqueue'
 import Dequeue from './Forms/Dequeue'
 
 const Queue = () => {
   const [queue, setQueue] = useState([])
-
   const [index, setIndex] = useState(0)
+  
+  const queueStyle =  {
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: "linear-gradient(#58C2ED, #1B85DC)",
+    width: '5rem',
+    height: '18rem',
+    bottom: '-2.5px'
+  }
  
   const onAdd = ({entry}) => {
+    if (entry == '') {
+      alert('Enter number')
+      return
+    }
+
+    if (queue.length > 9) {
+      alert('Queue is at max length')
+      return
+    }
+
+    if (Math.abs(entry) > 999) {
+      alert('Number must be 3 digits or less')
+      return
+    }
+
     let a = queue
     a.push({key: index, element: entry})
     setQueue(a)
@@ -22,11 +47,25 @@ const Queue = () => {
   }
   
   const transition = useTransition(queue, entry => entry.key, {
-    initial: {opacity: 0, display: 'inline'},
-    from: {opacity: 0, display: 'inline' },
-    enter: {opacity: 1, transition: 'opacity .5s'},
-    leave: {opacity: 0, transition: 'opacity .5s' }, 
+    initial: {opacity: 0},
+    from: {opacity: 0},
+    enter: {opacity: 1, transition: 'opacity .25s'},
+    leave: {opacity: 0, transition: 'opacity .25s', color: 'red'}
   })
+
+  // display queue with animation when added to or removed
+  const displayQueue = () => {
+    console.log("hello")
+    return (transition.map(({item, props, key}) =>         
+      <animated.div key={key} style={props}>
+        <li style={{listStyle: 'none'}}>
+          <Col style={Object.assign({border: 'solid', borderLeft: 0, left: `${queue.indexOf(item)*5}rem`}, queueStyle)} >
+            <h4 style={{color: 'white'}}>{item.element}</h4>
+          </Col>
+        </li>
+      </animated.div>
+    ))
+  }
 
   return (
     <Container>
@@ -36,11 +75,11 @@ const Queue = () => {
       </header>
       <Row><Enqueue enqueue={onAdd} /></Row>
       <Row><Dequeue dequeue={onDelete} /></Row>
-      <ul className='queue'>
-      {transition.map(({item, props, key}) => 
-        <animated.div key={key} style={props}><li style={{backgroundColor: 'red', borderRadius:'5px', padding:'5px'}}>{item.element}</li></animated.div>
-      )}
-      </ul>
+      <Row>
+        <div style={{position: 'relative', width: '50rem', height: '18rem', margin: '0 auto' , border: 'solid'}}>
+          <ul>{displayQueue()}</ul>
+        </div>
+      </Row>
     </Container>
   )}
 
