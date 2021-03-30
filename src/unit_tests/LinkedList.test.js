cfc0bb474167bc216a8b010d1bfa799161fab311
Justin Hurst <hurstjustin502@gmail.com>
2021-03-30 19:37:05 -0400

    fixed about page snapshot test

--- src/unit_tests/LinkedList.test.js
+++ src/unit_tests/LinkedList.test.js
@@ -1,8 +1,21 @@
+import '@testing-library/jest-dom'
 import React from 'react';
 import LinkedList from 'components/LinkedList';
 import renderer from 'react-test-renderer';
+import { render, fireEvent, getByTestId} from '@testing-library/react';
 
 test("Linked list page renders correctly", () => {
     const tree = renderer.create(<LinkedList/>).toJSON()
     expect(tree).toMatchSnapshot()
 });
+
+// Testing hooks
+it("insert button works", () => {
+    const { container } = render(<App />);
+    const insertButton = getByTestId(container, "submit");
+    const input = getByTestId(container, "Value");
+    const value = 'test';
+    fireEvent.change(input, { target: { value: value} });
+    fireEvent.click(insertButton);
+    expect()
+})
