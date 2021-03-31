b37569fd1203f998d36fc55efff185ef4a0cf6af
Jason Henry <jasdhenr@iu.edu>
2021-03-30 21:22:30 -0400

    added insertionsort and updated enqueue and dequeue to be used anywhere

--- src/components/InsertionSort.js
+++ src/components/InsertionSort.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 import {animated, useTransition} from 'react-spring'
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 import Enqueue from './Forms/Enqueue'
 import Dequeue from './Forms/Dequeue'
 
-const Queue = () => {
+const InsertionSort = () => {
   const [array, setArray] = useState([])
   const [index, setIndex] = useState(0)
-  
-  const queueStyle =  {
+
+  const arrayStyle =  {
     position: 'absolute',
     display: 'flex',
     alignItems: 'center',
@@ -18,59 +18,76 @@ const Queue = () => {
     height: '18rem',
     bottom: '-2.5px'
   }
-  
+
+  const updateArray = () => {
+      setArray(sort(array))
+  }
+
+  const sort = (array) => {
+    let a = [...array]
+    for (let j = 1; j < a.length; j++) {
+      let key = a[j]
+      let i = j - 1
+      while (i >= 0 && a[i].element > key.element) {
+        a[i + 1] = a[i]
+        i--
+      }
+      a[i + 1] = key
+    }
+    return a 
+  }
+
   // updates state of array 
   const onAdd = (entry) => {
-    setArray(enqueue(array, entry))
+    setArray(add(array, entry))
     setIndex(index + 1)
   }
 
   // returns array with number added to the end
-  const enqueue = (queue, number) => {
+  const add = (array, number) => {
     if (number == '') {
       alert('Enter number')
-      return queue
+      return array
     }
     
-    if (queue.length > 9) {
+    if (array.length > 9) {
       alert('Queue is at max length')
-      return queue
+      return array
     }
 
     if (Math.abs(number) > 999) {
       alert('Number must be 3 digits or less')
-      return queue
+      return array
     }
     
-    queue.push({key: index, element: number})
-    return queue
+    array.push({key: index, element: Number(number)})  
+    return array
   }
 
   // updates state of array
   const onDelete = () => {
-    setArray(dequeue(array))
+    setArray(remove(array))
     setIndex(index + 1)
   }
 
   // returns array with first element deleted
-  const dequeue = (queue) => {
-    return queue.splice(1)  
+  const remove = (array) => {
+    return array.splice(1)  
   }
-  
+
   const transition = useTransition(array, entry => entry.key, {
-    initial: {opacity: 0},
-    from: {opacity: 0},
-    enter: {opacity: 1, transition: 'opacity .25s'},
-    leave: {opacity: 0, transition: 'opacity .25s', color: 'red'}
+    initial: {opacity: 0, transition: 'opacity .5s'},
+    from: {opacity: 0, transition: 'opacity .5s'},
+    enter: {opacity: 1, transition: 'opacity .5s'},
+    leave: {opacity: 0, transition: 'opacity .5s'}
   })
 
-  // display array with animation when added to or removed
-  const displayQueue = () => {
+  const displayArray = () => {
     console.log("hello")
     return (transition.map(({item, props, key}) =>         
       <animated.div key={key} style={props}>
         <li style={{listStyle: 'none'}}>
-          <Col style={Object.assign({border: 'solid', borderLeft: 0, left: `${array.indexOf(item)*5}rem`}, queueStyle)} >
+          <Col style={Object.assign({border: 'solid', borderLeft: 0, left: `${array.indexOf(item)*5}rem`}, arrayStyle)} >
             <h4 style={{color: 'white'}}>{item.element}</h4>
           </Col>
         </li>
@@ -80,19 +97,17 @@ const Queue = () => {
 
   return (
     <Container>
-      <header className='header'>
-        <h1>Queue</h1>
-        <h1>FIFO: First In First Out</h1>
-      </header>
-      <Row><Enqueue enqueue={onAdd} title={'Enqueue'} /></Row>
-      <Row><Dequeue dequeue={onDelete} title={'Dequeue'}/></Row>
+      <Row><Enqueue enqueue={onAdd} title={'Insert'}/></Row>
+      <Row><Dequeue dequeue={onDelete} title={'Remove'}/></Row>
+      <Row><button className="my-1" onClick={updateArray}>Sort</button></Row>
       <Row>
         <div style={{position: 'relative', width: '50rem', height: '18rem', margin: '0 auto' , border: 'solid'}}>
-          <ul>{displayQueue()}</ul>
+          <ul>{displayArray()}</ul>
         </div>
       </Row>
     </Container>
   )
+
 }
 
-export default Queue
+export default InsertionSort
