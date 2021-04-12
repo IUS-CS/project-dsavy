1ed4f51503fe08075fb5a22c8268671746cca299
zkhussain <kzhzaid02@gmail.com>
2021-04-12 15:51:13 -0400

    add controls

--- src/components/Recursion.js
+++ src/components/Recursion.js
@@ -1,46 +1,61 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import { Form, Row, Col, Button, Container } from 'react-bootstrap';
+import {useTransition, animated} from 'react-spring';
 
-const Node = (props) => {
-    return (
-        <div style={{position: 'absolute', background: "linear-gradient(#58C2ED, #1B85DC)", width: '18rem', height: '3rem', border: 'solid', bottom: `${props.id*3}rem`, right: '-2.5px', borderBottom: '0'}}>
-            <h4 style={{textAlign: 'center', color: 'white', fontWeight: '550'}}>{props.text}</h4>
-        </div>
-    );
-}
+
 
 const Recursion = () => {
-    const [frames, setFrames] = useState([[]]) 
+    var [frames, setFrames] = useState([])
+    
+    const [isRunning, setIsRunning] = useState(false) // 'Visualize' is pressed or not
+    const [delay, setDelay] = useState(50) // initial speed is 50 (middle of slider)
+
+
+    
+
     function handleVis(event) {
         event.preventDefault()
+        setIsRunning(true)
+    }
+
+    function handleReset() {
+        window.location.reload()
+    }
+
+    function handlePause(event) {
+        event.preventDefault()
+        // if not paused, pause it, else reset it to the value on the slider
+        delay !== null ? setDelay(null) : setDelay(document.getElementById("speed-range").value)
     }
 
 
-    return(
-        <div>
-            <Container fluid style={{paddingTop:60, paddingLeft: 30}}>
 
+    return(
+        <Container fluid style={{paddingTop:30, paddingLeft: 30}}>
             <Row style={{justifyContent:'center'}}>
+                {/* Slider */}
+                <div className="my-3" style={{paddingLeft: 10}}>
+                    <label htmlFor="speed-range" style={{margin: '0 auto'}}>Speed</label>
+                    <input type="range" className="custom-range" id="speed-range" onChange={event => setDelay(event.target.value)} style={{transform: 'rotate(180deg)'}} />
+                </div>
+                <Form inline onSubmit={handlePause} style={{paddingLeft: 10}}>
+                    <Button type="submit" className="my-1" id="pause">||</Button>
+                </Form>
                 <Form inline onSubmit={handleVis} style={{paddingLeft: 20}}>
                     <Form.Control
                         as="select"
                         className="my-1 mr-sm-2"
                         name="method">
-                        <option value="0">Fibbonacci</option>
+                        <option value="0">Fibbonacci, n=4</option>
                     </Form.Control>
-                    <Button type="submit" className="my-1" style={{}}>Visualize</Button>
+                    <Button id="visualize" type="submit" className="my-1">Visualize</Button>
                 </Form>
-                <Form inline onSubmit={handleVis} style={{paddingLeft: 10}}>
-                    <Button type="submit" className="my-1" style={{}}>Reset</Button>
+                <Form inline onSubmit={handleReset} style={{paddingLeft: 10}}>
+                    <Button type="submit" className="my-1">Reset</Button>
                 </Form>
             </Row>
             
-            {/* this div holds the stack nodes */}
-            {/* <div id="stackbox" style={{width: '18rem', height: '28rem', position: 'relative', right: '9rem', bottom: '12rem', left: '42rem', marginBottom: '2rem', border: 'solid', borderTop: 0}}>
-                {stack}
-            </div> */}
         </Container>
-        </div>
     );
 }
 
